b210af39d94a0f7de83ab0c4e8c29c6d3cb775e6
FlyingMinion10 <zepedajuanfe9@gmail.com>
2025-09-22 23:28:01 -0600

    Diseño de la barra de busqueda

--- components/layout/navbar/search.tsx
+++ components/layout/navbar/search.tsx
@@ -1,21 +1,25 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useSearchParams } from "next/navigation";
 
 export default function Search() {
   const searchParams = useSearchParams();
 
   return (
-    <form action="/search" method="GET" className="w-max-[550px] relative w-full lg:w-80 xl:w-full">
+    <form
+      action="/search"
+      method="GET"
+      className="w-max-[550px] relative w-full lg:w-80 xl:w-full"
+    >
       <input
-        key={searchParams?.get('q')}
+        key={searchParams?.get("q")}
         type="text"
         name="q"
         placeholder="Escribe tu medida. Ej. 205/55 R16"
         autoComplete="off"
-        defaultValue={searchParams?.get('q') || ''}
-        className="text-md w-full rounded-lg border border-yellow-500 bg-white px-4 py-2 text-yellow-500 placeholder:text-yellow-400 md:text-sm dark:border-yellow-300 dark:bg-transparent dark:text-yellow-300 dark:placeholder:text-yellow-200"
+        defaultValue={searchParams?.get("q") || ""}
+        className="text-md w-full rounded-lg border bg-white px-4 py-2 text-black placeholder:text-neutral-500 md:text-sm focus:!border-white dark:focus:!border-white focus:outline-none focus:ring-0 transition-colors duration-150 dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
       />
       <button
         type="submit"
@@ -33,7 +37,7 @@ export function SearchSkeleton() {
     <form className="w-max-[550px] relative w-full lg:w-80 xl:w-full">
       <input
         placeholder="Search for products..."
-        className="w-full rounded-lg border border-yellow-500 bg-white px-4 py-2 text-sm text-yellow-500 placeholder:text-yellow-400 dark:border-yellow-300 dark:bg-transparent dark:text-yellow-300 dark:placeholder:text-yellow-200"
+        className="w-full rounded-lg border bg-white px-4 py-2 text-sm text-black placeholder:text-neutral-500 focus:!border-white dark:focus:!border-white focus:outline-none focus:ring-0 transition-colors duration-150 dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
       />
       <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
         <MagnifyingGlassIcon className="h-4" />
