28be6b16523ac90e0f70d02013a1b062c25ec910
FlyingMinion10 <zepedajuanfe9@gmail.com>
2026-02-12 17:49:41 -0600

    Product Page

--- app/product/[handle]/page.tsx
+++ app/product/[handle]/page.tsx
@@ -84,7 +84,7 @@ export default async function ProductPage(props: {
       />
       <div className="mx-auto max-w-(--breakpoint-2xl) px-4 mt-10 pt-20 ">
         <div className="flex flex-col rounded-lg border border-neutral-200 bg-white p-8 text-black md:p-12 lg:flex-row lg:gap-8 dark:border-neutral-200 dark:bg-white dark:text-black">
-          <div className="h-full w-full basis-full lg:basis-4/6">
+          <div className="h-full w-full basis-full lg:basis-3/6">
             <Suspense
               fallback={
                 <div className="relative aspect-square h-full max-h-[550px] w-full overflow-hidden" />
@@ -99,7 +99,7 @@ export default async function ProductPage(props: {
             </Suspense>
           </div>
 
-          <div className="basis-full lg:basis-2/6">
+          <div className="basis-full lg:basis-3/6">
             <Suspense fallback={null}>
               <ProductDescription product={product} />
             </Suspense>
