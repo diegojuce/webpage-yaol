7b63d6d8f36e8f0928a40434e6e94dad1d9417fa
FlyingMinion10 <zepedajuanfe9@gmail.com>
2026-04-24 18:27:15 -0600

    promo colinas

--- next.config.ts
+++ next.config.ts
@@ -8,4 +8,13 @@ export default {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [{ protocol: "https", hostname: "**", pathname: "/**" }],
   },
+  async redirects() {
+    return [
+      {
+        source: "/promo-colinas",
+        destination: "https://stg-back.yantissimo.com/qr/promo/colinas",
+        permanent: false,
+      },
+    ];
+  },
 };
